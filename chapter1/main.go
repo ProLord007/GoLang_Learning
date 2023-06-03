5412641f84231d69793919b8fc5acf7e6317b23b
Aurora-820 <aurora.galaxy0820@gmail.com>
2023-06-03 19:05:01 +0900

    Struct Learning

--- chapter1/main.go
+++ chapter1/main.go
@@ -1,14 +1,39 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+	"os"
+)
 
-func first() {
-	fmt.Println("1st")
+type Circle struct {
+	x, y, r float64
 }
-func second() {
-	fmt.Println("2nd")
+
+func (c Circle) power() float64 {
+	c.r = 2
+	return (c.r * c.r * math.Pi)
 }
 func main() {
-	defer second()
-	first()
+	c := Circle{x: 0, y: 0, r: 5}
+	file, err := os.Open("test.txt")
+	if err != nil {
+		return
+	}
+	defer file.Close()
+
+	//get the file size
+	stat, err := file.Stat()
+	if err != nil {
+		return
+	}
+	// read the file
+	bs := make([]byte, stat.Size())
+	_, err = file.Read(bs)
+	if err != nil {
+		return
+	}
+	str := string(bs)
+	fmt.Println(str)
+	fmt.Println(c.power())
 }
