11a3623fe1e8a8fca82d8e711251ea719f32f08d
Aurora-820 <aurora.galaxy0820@gmail.com>
2023-06-04 05:01:10 +0900

    Directory_Search

--- chapter1/main.go
+++ chapter1/main.go
@@ -2,33 +2,20 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
+	"os"
 )
 
 func main() {
-	/*	file, err := os.Open("test.txt")
-		if err != nil {
-			return
-		}
-		defer file.Close()
-
-		//get the file size
-		stat, err := file.Stat()
-		if err != nil {
-			return
-		}
-		// read the file
-		bs := make([]byte, stat.Size())
-		_, err = file.Read(bs)
-		if err != nil {
-			return
-		}
-		str := string(bs)
-		fmt.Println(str)*/
-	iobs, err := ioutil.ReadFile("test.txt")
-	if err == nil {
-		fmt.Println(string(iobs))
-	} else {
-		fmt.Println(err)
+	dir, err := os.Open(".")
+	if err != nil {
+		return
+	}
+	defer dir.Close()
+	fileInfos, err := dir.Readdir(-1)
+	if err != nil {
+		return
+	}
+	for _, fi := range fileInfos {
+		fmt.Println(fi.Name())
 	}
 }
