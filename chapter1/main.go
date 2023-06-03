27d3de8bb545952f07e03c2f4c6083f0d4a6928c
Aurora-820 <aurora.galaxy0820@gmail.com>
2023-06-04 05:31:03 +0900

    Bi-direction list

--- chapter1/main.go
+++ chapter1/main.go
@@ -1,14 +1,17 @@
 package main
 
 import (
+	"container/list"
 	"fmt"
-	"os"
-	"path/filepath"
 )
 
 func main() {
-	filepath.Walk(".", func(path string, info os.FileInfo, err error) error {
-		fmt.Println(path)
-		return nil
-	})
+	var x list.List
+	x.PushBack(1)
+	x.PushBack(2)
+	x.PushBack(3)
+
+	for e := x.Front(); e != nil; e = e.Next() {
+		fmt.Println(e.Value)
+	}
 }
