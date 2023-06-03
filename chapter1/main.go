599cbdd26129d4b87107f87c81be6fd0954a99cc
Aurora-820 <aurora.galaxy0820@gmail.com>
2023-06-04 04:50:31 +0900

    File ReadWrite

--- chapter1/main.go
+++ chapter1/main.go
@@ -2,27 +2,33 @@ package main
 
 import (
 	"fmt"
-	"os"
+	"io/ioutil"
 )
 
 func main() {
-	file, err := os.Open("test.txt")
-	if err != nil {
-		return
-	}
-	defer file.Close()
+	/*	file, err := os.Open("test.txt")
+		if err != nil {
+			return
+		}
+		defer file.Close()
 
-	//get the file size
-	stat, err := file.Stat()
-	if err != nil {
-		return
+		//get the file size
+		stat, err := file.Stat()
+		if err != nil {
+			return
+		}
+		// read the file
+		bs := make([]byte, stat.Size())
+		_, err = file.Read(bs)
+		if err != nil {
+			return
+		}
+		str := string(bs)
+		fmt.Println(str)*/
+	iobs, err := ioutil.ReadFile("test.txt")
+	if err == nil {
+		fmt.Println(string(iobs))
+	} else {
+		fmt.Println(err)
 	}
-	// read the file
-	bs := make([]byte, stat.Size())
-	_, err = file.Read(bs)
-	if err != nil {
-		return
-	}
-	str := string(bs)
-	fmt.Println(str)
 }
