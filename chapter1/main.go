fbd8c5f62a6b61ae3144c87d1dd0df953eadbeaa
Aurora-820 <aurora.galaxy0820@gmail.com>
2023-06-03 13:57:34 +0900

    I am.

--- chapter1/main.go
+++ chapter1/main.go
@@ -1,17 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
-func f() {
-	fmt.Println("asdfasdf")
+func f(n int) bool {
+	if n <= 1 {
+		return false
+	}
+	for i := 2; i*i <= int(math.Sqrt(float64(n))); i++ {
+		if n%i == 0 {
+			return false
+		}
+	}
+	return true
 }
 
 func main() {
-	var a string = "asdf"
 	var b int = 5
-	var c float64
-	b1 := 3.5
-	fmt.Scanf("%f", &c)
-	fmt.Println(a, b, b1, c)
-	f()
+	fmt.Scanf("%d", &b)
+	fmt.Println(f(b))
 }
