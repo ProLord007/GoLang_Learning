8ecefe920e2f82353decf35c864144160fbba02e
Aurora-820 <aurora.galaxy0820@gmail.com>
2023-06-03 16:07:33 +0900

    HashMap learn

--- chapter1/main.go
+++ chapter1/main.go
@@ -1,27 +1,19 @@
 package main
 
-import (
-	"fmt"
-	"math"
-)
+import "fmt"
 
-func f(n int) bool {
-	if n <= 1 {
-		return false
-	}
-	for i := 2; i*i <= int(math.Sqrt(float64(n))); i++ {
-		if n%i == 0 {
-			return false
-		}
-	}
-	return true
-}
 func main() {
-	arr := []int{1, 2, 3, 4, 5}
-	fmt.Println("Enter 5 integers:")
-	arr = append(arr, 2, 3, 4, 5, 6, 7, 989, 0)
-	fmt.Println("Array elements are:")
-	for i := 0; i < len(arr); i++ {
-		fmt.Println(arr[i])
-	}
+	elements := make(map[string]string)
+	elements["H"] = "Hydrogen"
+	elements["He"] = "Helium"
+	elements["Li"] = "Lithium"
+	elements["Be"] = "Beryllium"
+	elements["B"] = "Boron"
+	elements["C"] = "Carbon"
+	elements["N"] = "Nitrogen"
+	elements["O"] = "Oxygen"
+	elements["F"] = "Fluorine"
+	elements["Ne"] = "Neon"
+	name, ok := elements["Su"]
+	fmt.Println(name, ok)
 }
