d17d0d9db80276e96b485f2c29fed0b87f3becc3
Aurora-820 <aurora.galaxy0820@gmail.com>
2023-06-04 11:03:58 +0900

    TCP connection

--- chapter1/main.go
+++ chapter1/main.go
@@ -1,16 +1,59 @@
 package main
 
 import (
+	"encoding/gob"
 	"fmt"
-	"hash/crc32"
+	"net"
 )
 
+func server() {
+	// listen on a port
+	ln, err := net.Listen("tcp", ":9999")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	for {
+		// accept a connection
+		c, err := ln.Accept()
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+		// handle the connection
+		go handleServerConnection(c)
+	}
+}
+func handleServerConnection(c net.Conn) {
+	// receive the message
+	var msg string
+	err := gob.NewDecoder(c).Decode(&msg)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println("Received", msg)
+	}
+	c.Close()
+}
+func client() {
+	// connect to the server
+	c, err := net.Dial("tcp", "127.0.0.1:9999")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	// send the message
+	msg := "Hello, World"
+	fmt.Println("Sending", msg)
+	err = gob.NewEncoder(c).Encode(msg)
+	if err != nil {
+		fmt.Println(err)
+	}
+	c.Close()
+}
 func main() {
-	// create a hasher
-	h := crc32.NewIEEE()
-	// write our data to it
-	h.Write([]byte("test"))
-	// calculate the crc32 checksum
-	v := h.Sum32()
-	fmt.Println(v)
+	go server()
+	go client()
+	var input string
+	fmt.Scanln(&input)
 }
