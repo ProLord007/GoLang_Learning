462023010dd18ff217c921ee49b60cd1b9423dbf
Aurora-820 <aurora.galaxy0820@gmail.com>
2023-06-04 14:41:11 +0900

    CHannel

--- chapter1/main.go
+++ chapter1/main.go
@@ -1,59 +1,27 @@
 package main
 
 import (
-	"encoding/gob"
 	"fmt"
-	"net"
+	"strconv"
+	"time"
 )
 
-func server() {
-	// listen on a port
-	ln, err := net.Listen("tcp", ":9999")
-	if err != nil {
-		fmt.Println(err)
-		return
+func pinger(c chan string) {
+	for i := 0; ; i++ {
+		c <- "ping" + strconv.Itoa(i)
 	}
+}
+func printer(c chan string) {
 	for {
-		// accept a connection
-		c, err := ln.Accept()
-		if err != nil {
-			fmt.Println(err)
-			continue
-		}
-		// handle the connection
-		go handleServerConnection(c)
+		msg := <-c
+		fmt.Println(msg)
+		time.Sleep(time.Second * 1)
 	}
-}
-func handleServerConnection(c net.Conn) {
-	// receive the message
-	var msg string
-	err := gob.NewDecoder(c).Decode(&msg)
-	if err != nil {
-		fmt.Println(err)
-	} else {
-		fmt.Println("Received", msg)
-	}
-	c.Close()
-}
-func client() {
-	// connect to the server
-	c, err := net.Dial("tcp", "127.0.0.1:9999")
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	// send the message
-	msg := "Hello, World"
-	fmt.Println("Sending", msg)
-	err = gob.NewEncoder(c).Encode(msg)
-	if err != nil {
-		fmt.Println(err)
-	}
-	c.Close()
 }
 func main() {
-	go server()
-	go client()
+	var c chan string = make(chan string)
+	go pinger(c)
+	go printer(c)
 	var input string
 	fmt.Scanln(&input)
 }
